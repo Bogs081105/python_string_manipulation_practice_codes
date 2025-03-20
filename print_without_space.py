93487dda490114e9c2849b6ce1d4bf89ada2cd9e
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 19:44:40 +0800

    added a function which removes the spaces inside the string

--- print_without_space.py
+++ print_without_space.py
@@ -1,4 +1,7 @@
 #ask the user to input the name with spaces at the beginning
 #create a variable to modify the input
 full_name = input("Enter your full name with spaces at the beginning: ")
+
 #print the input without spaces
+no_spaces = full_name.lstrip
+print(no_spaces)
