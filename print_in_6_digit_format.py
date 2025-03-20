0f19aca98fdd006eac22100b05d6f912a3086c79
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 20:06:35 +0800

    added a function that adds zeros until it reaches 6 digits

--- print_in_6_digit_format.py
+++ print_in_6_digit_format.py
@@ -3,3 +3,8 @@
 six_digits = input("Input a number from 0-1000")
 #add zeros at the beginning until it becomes 6 digits
 #print the numbers
+if six_digits < len(6):
+    six_digits.zfill(6)
+    print(six_digits)
+else:
+    ("Please enter a number from 0-1000")
