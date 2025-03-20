baa4b9c8d44fd9bfdcad11974fd315a1b09b8b51
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:25:29 +0800

    added the function to count the number of characters

--- print_number_of_characters.py
+++ print_number_of_characters.py
@@ -1,5 +1,8 @@
 #ask the user to input their full name
 #store it inside the variable
 full_name = input("Please enter your full name: ")
+
 #modify the variable to count the number of characters
+#store inside another variable
 #print the number of characters
+print(f"The number of characters in your name is: {len(full_name)}")
