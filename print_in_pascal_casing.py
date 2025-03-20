670d1c7619001427bd60f207e4c9d43ee8f7404a
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:31:46 +0800

    pseudo code created

---
+++ print_in_pascal_casing.py
@@ -0,0 +1,4 @@
+#ask the user to input their name in improper casing
+#store it inside a variable
+#modify the variable into pascal casing, remove the spaces and organize the casing
+#print the result
