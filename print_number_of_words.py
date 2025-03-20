cfe3515dfc450c3629a2e7c6af89f30622f22a6d
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:10:38 +0800

    rearranged pseudo codes and added print function

--- print_number_of_words.py
+++ print_number_of_words.py
@@ -1,6 +1,10 @@
 #ask the user to input a complete statement
 #store inside a variable
 statement = input("Please enter a complete statement: ")
+
+#add another variable to store the modified variable
 #modify the variable to count the words
 word_count = len(statement.split())
+
 #print the result
+print("The amount of words inside the statements are: ")
