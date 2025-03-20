81478f924c76336764fbaf7e8ca8b40941e14cca
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 20:24:51 +0800

    fixed add zeros function again in a proper format and fixed the print please enter a number from 0-1000 function

--- print_in_6_digit_format.py
+++ print_in_6_digit_format.py
@@ -1,9 +1,10 @@
 #ask the user to input a number from 0-1000
 #store it inside a variable
 six_digits = input("Input a number from 0-1000: ")
+
 #add zeros at the beginning until it becomes 6 digits
 #print the numbers
-if six_digits.isdigit() and 0 <= int(six_digits) <= 1000:
+if six_digits.isdigit() and len(six_digits) < 6:
     print(six_digits.zfill(6))
-else:
-    ("Please enter a number from 0-1000")
+elif six_digits.isdigit() and len(six_digits) >= 6:
+    print("Please enter a number from 0-1000")
