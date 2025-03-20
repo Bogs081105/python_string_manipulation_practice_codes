070a28445b944ee3528c95de1427b1e8159ae389
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 20:07:30 +0800

    added space and colon to make the input function more proper

--- print_in_6_digit_format.py
+++ print_in_6_digit_format.py
@@ -1,6 +1,6 @@
 #ask the user to input a number from 0-1000
 #store it inside a variable
-six_digits = input("Input a number from 0-1000")
+six_digits = input("Input a number from 0-1000: ")
 #add zeros at the beginning until it becomes 6 digits
 #print the numbers
 if six_digits < len(6):
