08b52258507f0b8de19a3a0a5bebf1cf67f9d581
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:38:55 +0800

    added full name function

--- print_in_snake_case.py
+++ print_in_snake_case.py
@@ -1,5 +1,6 @@
 #ask the user to input their name in improper casing
 #store inside the variable
+full_name = input("Please enter your full name in improper casing: ")
 #modify the variables to lowecase
 #modify to replace blank spaces to _
 #print the result
