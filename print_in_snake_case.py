19b3725975a9035af94cd629a637af659368c7d1
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:42:57 +0800

    added a function that lowercases the input, and replaces spaces with underscores

--- print_in_snake_case.py
+++ print_in_snake_case.py
@@ -1,6 +1,8 @@
 #ask the user to input their name in improper casing
 #store inside the variable
 full_name = input("Please enter your full name in improper casing: ")
-#modify the variables to lowecase
+
+#modify the variables to lowercase
 #modify to replace blank spaces to _
 #print the result
+print(full_name.lower().replace(" ","_"))
