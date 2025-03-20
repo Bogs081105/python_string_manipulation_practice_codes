9600e8803ea5e450ec42192df9d99d781d38b79d
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 20:28:46 +0800

    added a text to the result to make the code more understandable on what it does

--- print_in_6_digit_format.py
+++ print_in_6_digit_format.py
@@ -5,6 +5,6 @@ six_digits = input("Input a number from 0-1000: ")
 #add zeros at the beginning until it becomes 6 digits
 #print the numbers
 if six_digits.isdigit() and len(six_digits) < 6:
-    print(six_digits.zfill(6))
+    print(f"The number in six digits is {six_digits.zfill(6)}")
 elif six_digits.isdigit() and len(six_digits) >= 6:
     print("Please enter a number from 0-1000")
