150ce38effac468a4d971f9b28986438d371e736
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:01:12 +0800

    added input function

--- print_in_reverse_casing.py
+++ print_in_reverse_casing.py
@@ -1,4 +1,5 @@
 #ask the user to input their name in improper casing
 #store inside the variable
+full_name = input("Please enter your full name with improper casing: ")
 #modify the variable
 #print the result
