a2a0238410c4263a488c5dae7c62984f2894a5e0
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:02:49 +0800

    added swapcase function

--- print_in_reverse_casing.py
+++ print_in_reverse_casing.py
@@ -1,5 +1,7 @@
 #ask the user to input their name in improper casing
 #store inside the variable
 full_name = input("Please enter your full name with improper casing: ")
+
 #modify the variable
 #print the result
+print(full_name.swapcase())
