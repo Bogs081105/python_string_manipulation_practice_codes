ee6868e0a784802d018a633a4bbe327c7fb74fb4
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 19:40:12 +0800

    added a function which asks the user for their full name with leading spaces

--- print_without_space.py
+++ print_without_space.py
@@ -1,3 +1,4 @@
 #ask the user to input the name with spaces at the beginning
 #create a variable to modify the input
+full_name = input("Enter your full name with spaces at the beginning: ")
 #print the input without spaces
