cd0234148d7f61063a4b14b31ec8ed70b28301d1
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-20 21:34:16 +0800

    added the function to print in pascal) '

--- print_in_pascal_casing.py
+++ print_in_pascal_casing.py
@@ -1,5 +1,7 @@
 #ask the user to input their name in improper casing
-full_name = input("Enter your full name in improper casing: ")
 #store it inside a variable
+full_name = input("Enter your full name in improper casing: ")
+
 #modify the variable into pascal casing, remove the spaces and organize the casing
 #print the result
+print(full_name.title().replace(" ", ""))
